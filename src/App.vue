cc2120b508f089e53193843a236ac8b9d381c2e4
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-11 15:32:37 +0100

    styling cards

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script setup>
-import {ref} from "vue"
+
 import {RouterView} from "vue-router"
 import Nav from "./components/Nav.vue"
 
-const randomState = ref("")
+
 </script>
 
 
 
 <template>
   <div>
-    {{randomState}}
-    <button @click="randomState = 'Hello there' ">click</button>
+
     <Nav />
     <RouterView/>
   </div>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -6,7 +6,14 @@
 
 <template>
     <div>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink active-class="active" to="/">Home</RouterLink>
+        <RouterLink active-class="active" to="/about">About</RouterLink>
     </div>
 </template>
+
+<style scoped>
+    .active {
+        font-weight: 900;
+        color: gold
+    }
+</style>
