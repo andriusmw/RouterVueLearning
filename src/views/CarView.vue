1fc3d91d30f5d26e0f525b5614cbd064d9addce3
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-11 15:54:52 +0100

    showing info of each car

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,5 +1,18 @@
+<script setup>
+    import {useRoute} from "vue-router"
+    import cars from "../data/cars.json"
+
+    const route = useRoute()
+    const car = cars.find(c => c.id === parseInt(route.params.id))
+    // Busca en el array de coches, el coche cuyo id es el que llega en los params
+
+</script>
+
 <template>
     <div>
         <h1>Car view</h1>
+        <p>{{car.name}}</p>
+         <p>{{car.year}}</p>
+          <p>{{car.price}}</p>
     </div>
 </template>
