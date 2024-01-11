6742be3f3b8005b3708d673c26226de5cad85ee4
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-11 15:09:59 +0100

    routerLink

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
+import {ref} from "vue"
 import {RouterView} from "vue-router"
+import Nav from "./components/Nav.vue"
 
-
-
+const randomState = ref("")
 </script>
 
 
 
 <template>
   <div>
+    {{randomState}}
+    <button @click="randomState = 'Hello there' ">click</button>
+    <Nav />
     <RouterView/>
   </div>
 </template>
